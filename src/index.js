34a3dcf200acd8eb4c02356340fedf76e8c3e0c1
jaemin.han <jaemin.han@nyu.edu>
2018-03-30 18:20:54 -0400

    render all three components

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ export class OlderCoaster extends React.Component {
   render() {
     return React.createElement('div', { className: 'oldercoaster' },
       React.createElement('p', {}, 'Two grannies having the time of their life!'),
-      React.createElement('p', {}, 'Passengers'),
+      React.createElement('p', {}, 'Passengers:'),
       React.createElement('ul', {}, 
         [
           React.createElement('li', {}, 'Agnes'),
@@ -22,35 +22,41 @@ export class OlderCoaster extends React.Component {
       )
     ) 
   }
-}
+};
 
 export class InFrontOfYou extends React.Component {
   render() {
     return React.createElement('div', {},
-      React.createElement('p', {}, `You shouldn't look too far`),
-      React.createElement('p', {}, 'sometimes, the solution is right in front of you.')
+      React.createElement('p', {}, `You shouldn't look too far.`),
+      React.createElement('p', {}, 'Sometimes, the solution is right in front of you.')
     )
   }
-}
+};
 
 export class ButcherShop extends React.Component {
   render() {
     return React.createElement('div', { className: 'butcher-shop' },
       React.createElement('p', {}, 'Hello! We have the following products for sale today:'),
       React.createElement('ul', {},
-        React.createElement('li', {}, 'Tenderloin'),
-        React.createElement('li', {}, 'Short ribs'),
-        React.createElement('li', {}, 'Beef shin'), 
-        React.createElement('li', {}, 'Ribeye')
-      ) 
+        // React.createElement('li', {}, 'Tenderloin'),
+        // React.createElement('li', {}, 'Short ribs'),
+        // React.createElement('li', {}, 'Beef shin'), 
+        // React.createElement('li', {}, 'Ribeye')
+        BUTCHER_PRODUCTS.map((product) => {
+          return React.createElement('li', {}, product)
+        })
+        //.map to filter butcher products in react.createElement format.
     )
-  }
-}
+  )}
+};
 
 
 ReactDOM.render(
-  <div>
-    render your components here! Refer back to the previous README if you are lost
-  </div>,
+  React.createElement('div', {}, [
+    React.createElement(OlderCoaster),
+    React.createElement(InFrontOfYou),
+    React.createElement(ButcherShop)
+  ]),
   document.getElementById('root')
-)
+);
+// Multiple class components -- inside 'div' create three or how many needed to render those components.
