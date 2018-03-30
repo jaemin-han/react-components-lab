0dc76d0c79a83cdf43881d0c5f247867b1fe4c42
jaemin.han <jaemin.han@nyu.edu>
2018-03-30 18:03:21 -0400

    third class component

--- src/index.js
+++ src/index.js
@@ -34,7 +34,11 @@ export class InFrontOfYou extends React.Component {
 }
 
 export class ButcherShop extends React.Component {
-
+  render() {
+    return React.createElement('div', { className: 'butcher-shop' },
+    React.createElement('p', {}, 'Hello! We have the following products for sale today:') 
+  )
+  }
 }
 
 
