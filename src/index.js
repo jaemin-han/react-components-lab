a017dd997475dd2616e25a21eab106e898684903
jaemin.han <jaemin.han@nyu.edu>
2018-03-30 17:37:07 -0400

    Initiate setup

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const BUTCHER_PRODUCTS = [
+  'Tenderloin',
+  'Short ribs',
+  'Beef shin',
+  'Ribeye'
+]
+
+// Define these exported classes
+export class OlderCoaster extends React.Component {
+  // your code here
+}
+
+export class InFrontOfYou extends React.Component {
+  // your code here
+}
+
+export class ButcherShop extends React.Component {
+  // your code here
+}
+
+
+ReactDOM.render(
+  <div>
+    render your components here! Refer back to the previous README if you are lost
+  </div>,
+  document.getElementById('root')
+)
